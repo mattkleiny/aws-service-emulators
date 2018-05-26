a4d252dd524e86312c9a73421eee648ebaab66e7
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-05-26 15:02:15 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Service Emulators
+
+Executable and embeddable emulators for AWS services for use in local testing and development.
---
+++ src/Amazon.Emulators/IAmazonServiceEmulatorHost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+
+namespace Amazon.Emulators
+{
+  public interface IAmazonServiceEmulatorHost<TService> : IDisposable
+    where TService : class, IAmazonService
+  {
+    Task Run(CancellationToken cancellationToken = default);
+  }
+}
