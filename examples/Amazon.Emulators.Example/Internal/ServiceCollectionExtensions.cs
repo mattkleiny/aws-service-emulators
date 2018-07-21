91a5179724b891031b52275ae328b0b6d583312e
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-07-21 13:25:27 +1000

    Stub an example project.

---
+++ examples/Amazon.Emulators.Example/Internal/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Amazon.Emulators.Example.Internal
+{
+  internal static class ServiceCollectionExtensions
+  {
+    public static IServiceCollection ReplaceWith<TService, TImplementation>(this IServiceCollection services) 
+      where TService : class 
+      where TImplementation : class, TService
+    {
+      services.RemoveAll<TService>();
+      services.AddSingleton<TService, TImplementation>();
+
+      return services;
+    }
+  }
+}
---
+++ examples/Amazon.Emulators.Example/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using Amazon.Emulators.Embedded;
+using Amazon.Emulators.Example.Internal;
+using Amazon.Lambda.Hosting;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Amazon.Emulators.Example
+{
+  public sealed class Startup
+  {
+    [LambdaFunction("producer")]
+    public async void Producer(IAmazonSQS sqs, CancellationToken cancellationToken = default)
+    {
+      var queueUrl = (await sqs.GetQueueUrlAsync("test-queue", cancellationToken)).QueueUrl;
+
+      while (!cancellationToken.IsCancellationRequested)
+      {
+        var request = new SendMessageRequest
+        {
+          QueueUrl    = queueUrl,
+          MessageBody = "Hello, World!"
+        };
+
+        await sqs.SendMessageAsync(request, cancellationToken);
+      }
+    }
+
+    [LambdaFunction("consumer")]
+    public async void Consumer(IAmazonSQS sqs, CancellationToken cancellationToken = default)
+    {
+      var queueUrl = (await sqs.GetQueueUrlAsync("test-queue", cancellationToken)).QueueUrl;
+
+      while (!cancellationToken.IsCancellationRequested)
+      {
+        var request = new ReceiveMessageRequest
+        {
+          QueueUrl            = queueUrl,
+          MaxNumberOfMessages = 10,
+          WaitTimeSeconds     = 5
+        };
+
+        var batch = await sqs.ReceiveMessageAsync(request, cancellationToken);
+
+        foreach (var message in batch.Messages)
+        {
+          Console.WriteLine(message);
+        }
+      }
+    }
+
+    [UsedImplicitly]
+    public void ConfigureServices(IServiceCollection services, IHostingEnvironment environment)
+    {
+      services.AddSQS();
+      services.AddFunctionalHandlers<Startup>();
+
+      services.ConfigureHostingOptions(options =>
+      {
+        if (environment.IsDevelopment())
+        {
+          services.ReplaceWith<IAmazonSQS, EmbeddedAmazonSQS>();
+        }
+      });
+    }
+  }
+}
