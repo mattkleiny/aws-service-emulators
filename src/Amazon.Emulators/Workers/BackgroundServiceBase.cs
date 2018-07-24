8cccd0e36a81db80ffad8b636f14b0487197cfa0
Matt Kleinschafer <matt.kleinschafer@xero.com>
2018-07-24 12:20:38 +1000

    Stub some work on background services for notifications.

---
+++ src/Amazon.Emulators/Workers/BackgroundServiceBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Amazon.Emulators.Workers
+{
+  /// <summary>A <see cref="Microsoft.Extensions.Hosting.BackgroundService"/> specialization that supports a single long-running background worker.</summary>
+  public abstract class BackgroundServiceBase : BackgroundService
+  {
+    public sealed override Task StartAsync(CancellationToken cancellationToken)
+    {
+      return base.StartAsync(cancellationToken);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+      return Task.Factory.StartNew(
+        () => WorkAsync(cancellationToken).Wait(cancellationToken),
+        cancellationToken,
+        TaskCreationOptions.LongRunning,
+        TaskScheduler.Default
+      );
+    }
+
+    public sealed override Task StopAsync(CancellationToken cancellationToken)
+    {
+      return base.StopAsync(cancellationToken);
+    }
+
+    protected abstract Task WorkAsync(CancellationToken cancellationToken);
+  }
+}
---
+++ src/Amazon.Emulators/Workers/NotifyingBackgroundService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amazon.Emulators.Workers
+{
+  /// <summary>Permits dispatching of <see cref="NotifyingBackgroundService.Notification"/>s to listeners. </summary>
+  public delegate Task NotificationDispatcher(NotifyingBackgroundService.Notification notification, CancellationToken cancellationToken = default);
+
+  /// <summary>A <see cref="BackgroundServiceBase"/> that supports a notification process.</summary>
+  public abstract class NotifyingBackgroundService : BackgroundServiceBase
+  {
+    private readonly BlockingCollection<Notification> notifications = new BlockingCollection<Notification>();
+    private readonly NotificationDispatcher           dispatcher;
+
+    protected NotifyingBackgroundService(NotificationDispatcher dispatcher)
+    {
+      Check.NotNull(dispatcher, nameof(dispatcher));
+
+      this.dispatcher = dispatcher;
+    }
+
+    /// <summary>Enqueues a <see cref="Notification"/> for dispatch.</summary>
+    protected void EnqueueNotification(Notification notification)
+    {
+      Check.NotNull(notification, nameof(notification));
+
+      notifications.Add(notification);
+    }
+
+    protected sealed override async Task WorkAsync(CancellationToken cancellationToken)
+    {
+      while (!cancellationToken.IsCancellationRequested)
+      {
+        var notification = notifications.Take(cancellationToken);
+
+        await dispatcher(notification, cancellationToken);
+      }
+    }
+
+    /// <summary>Encapsulates a notification to be dispatched by the service.</summary>
+    public sealed class Notification
+    {
+      public DateTime SendTime { get; } = DateTime.Now;
+      public object   Payload  { get; set; }
+    }
+  }
+}
